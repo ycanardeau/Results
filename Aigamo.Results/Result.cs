b7b7d99ab5f4cfb00b6c61c28ab70750b482285b
Aigamo <51428094+ycanardeau@users.noreply.github.com>
2026-09-17 08:24:58 +0900

    Add implicit conversion from error value to Result

--- Aigamo.Results/Result.cs
+++ Aigamo.Results/Result.cs
@@ -10,11 +10,19 @@ public closed record Result<T, TError>
 	private Result() { }
 
 	public sealed record Ok(T ResultValue) : Result<T, TError>;
+
 	public sealed record Error(TError ErrorValue) : Result<T, TError>;
+
+	public static implicit operator Result<T, TError>(T resultValue) => new Ok(resultValue);
+
+	public static implicit operator Result<T, TError>(TError errorValue) => new Error(errorValue);
 }
 
 public static class Result
 {
-	public static Result<T, TError> Ok<T, TError>(T resultValue) => new Result<T, TError>.Ok(resultValue);
-	public static Result<T, TError> Error<T, TError>(TError errorValue) => new Result<T, TError>.Error(errorValue);
+	public static Result<T, TError> Ok<T, TError>(T resultValue) =>
+		new Result<T, TError>.Ok(resultValue);
+
+	public static Result<T, TError> Error<T, TError>(TError errorValue) =>
+		new Result<T, TError>.Error(errorValue);
 }
